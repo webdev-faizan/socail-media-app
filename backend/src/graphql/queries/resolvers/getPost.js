e29e7bfea15179d1d15e9a4038445b25572b2bce
webdev-faizan <webdev.faizanali@gmail.com>
2024-02-25 07:53:58 -0500

    Removed unused file

--- backend/src/graphql/queries/resolvers/getPost.js
+++ backend/src/graphql/queries/resolvers/getPost.js
@@ -1,19 +1,35 @@
 import { PostModel } from '../../../model/postModel.js'
 
-export const getAllPostsResolver = async (_, { page = 1, limit = 10 }) => {
-  const pageNo = parseInt(page)
+export const getAllPostsResolver = async (
+  _,
+  { page = 1, limit = 10 },
+  context,
+) => {
+  const { error } = await ProtectRoutes(context)
+  if (error) {
+    throw new GraphQLError('Session has expired', {
+      extensions: {
+        code: 'BAD_REQUEST',
+        http: {
+          status: 400,
+        },
+      },
+    })
+  }
   const pageSize = parseInt(limit)
-  const validPage = isNaN(pageNo) || pageNo < 1 ? 1 : pageNo
+  const pageNo = parseInt(page) || 1
+  const validPage = pageNo > 1 ? pageNo : 1
+  // const validPage = isNaN(pageNo) || pageNo < 1 ? 1 : pageNo
   const skip = (validPage - 1) * pageSize
-
   const allPosts = await PostModel.find({})
     .populate('postOwner', 'firstName lastName email _id')
     .sort({ createdAt: -1 })
     .skip(skip)
     .limit(limit)
+    .lean()
 
   return {
-    message: 'nice',
+    message: 'Posts fetched successfully',
     data: allPosts,
   }
 }
